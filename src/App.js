cb881aa0d306f3d5f56fa3a326754b34af5fe024
Pedro <pedro7valla@hotmail.com>
2022-01-13 15:01:35 -0500

    Ejercicio cambia clases a funciones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import PersonCard from './components/PersonCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard firstName= "Pedro" lastName = "Valladares" age = {28} hColor = {"Black"}/>
+      <PersonCard firstName= "Jane" lastName = "Doe" age = {45} hColor = {"Black"}/>
+      <PersonCard firstName= "John" lastName = "Smith" age = {88} hColor = {"Brown"}/>
+      <PersonCard firstName= "Maria" lastName = "Smith" age = {62} hColor = {"Black"}/>
     </div>
   );
 }
